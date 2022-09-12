ea41ab6ed5e6ce6e0c78088cb6464d890ee6e09f
Christian <christian@maikens-imac.home>
2022-09-12 09:15:56 -0400

    app.js

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const express = require("express");
 // Bring in body parser so that we can parse the POST request body
 const bodyParser = require("body-parser");
 
-// Initiallize the Server and Port
+// Initialize the Server and Port
 const app = express();
 const port = 3000;
 
 // The following 2 lines are called middleware and they modify the request before it gets to our routes
 // so that we can properly access the values in the request body (req.body)
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false })); //******  Why is this set to False?
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
@@ -19,22 +19,22 @@ app.use(bodyParser.json());
 let globalFirstName = 2;
 let globalLastName = null;
 
-// Define the default server ROUTE (aka "/") for our server
+// Define the default server route (aka "/") for our server
 app.get("/", (req, res) => {
   //req stands for request and has the request user info on it such as the url params and http headers
   //res stands for response and has the methods used for responding to a request on it
-  console.log("Default Route");
+  console.log("default route");
 
-  const myName = "Christian Benites";
+  const myName = "James Nissenbaum";
   const today = new Date();
-  const todayBasic = today.toLocaleDateString();
+  const todayFormatted = today.toLocaleDateString();
   console.log("Today ", today);
-  res.send(`My Name is ${myName}. Today is ${todayBasic}`);
+  res.send(`My Name: ${myName}. Today's date: ${todayFormatted}`);
 });
 
 // This route will get the user's info from the query params and assign those values to the global variables
 // Example url: http://localhost:4000/save-user-info?firstName=Timmy&lastName=Turner
-app.post("/save-user-info", (req, res) => {
+app.get("/save-user-info", (req, res) => {
   // req.query is an object containing key/value pairs of the query params entered into the url after the ?
   console.log(req.query);
 
@@ -47,7 +47,9 @@ app.post("/save-user-info", (req, res) => {
   globalLastName = queryParamLastName;
 
   // This res.send() will always send the user info since queryParamFirstName and queryParamLastName are in this route handler function scope
-  res.send("User Info => " + "Name: " + queryParamFirstName + " " + queryParamLastName);
+  res.send(
+    "User Info => " + "Name: " + queryParamFirstName + " " + queryParamLastName
+  );
 });
 
 app.get("/show-user-info", (req, res) => {
@@ -55,35 +57,89 @@ app.get("/show-user-info", (req, res) => {
   res.send("User Info => " + "Name: " + globalFirstName + " " + globalLastName);
 });
 
-// An example route for sending a simple string
-app.get("/hello-class", (req, res) => {
-  console.log("Hello Class Route");
-  res.send("Hello Class!!!");
-});
-
-//get all movies in our movie list
-app.get("/all-movies", (req, res) => {
-  //res.send only sends strings. From now on, we want to use res.json to send JSON objects or JS arrays
-  res.json(favoriteMovieList);
-});
+// Movie CRUD Functions
 
 // For the assignment, make sure favoriteMovieList is in the global scope
-const favoriteMovieList = ["Matrix", "Bourne", "Another Movie"];
+const favoriteMovieList = [
+  {
+    title: "Star Wars",
+    starRating: 5,
+    isRecommended: true,
+    createdAt: new Date(),
+    lastModified: new Date(),
+  },
+  {
+    title: "The Avengers",
+    starRating: 4,
+    isRecommended: true,
+    createdAt: new Date(),
+    lastModified: new Date(),
+  },
+];
 
-// Movie CRUD Functions
 // Create
 
 // Post a new movie into the movies array
 app.post("/new-movie", (req, res) => {
-  //We'll use req.body to get the body payload from the post request that contains our new movie
-  console.log(req.body);
+  console.log("POST to /new-movie");
 
-  const newMovieTitle = req.body.title;
-  favoriteMovieList.push(newMovieTitle);
+  //We'll use req.body to get the body payload from the post request that contains our new movie
+  console.log("req.body ", req.body);
+
+  const newMovie = {
+    title: "",
+    starRating: 0,
+    isRecommended: false,
+    createdAt: new Date(),
+    lastModified: new Date(),
+  };
+
+  if (req.body.title === undefined) {
+    // Should trigger when req.body.title is undefined
+    console.log("title is not defined");
+    res.json({
+      success: false,
+      message: "title is a required field",
+    });
+    return;
+  } else {
+    console.log("title is defined");
+    newMovie.title = req.body.title;
+  }
+
+  if (req.body.starRating === undefined) {
+    // Should trigger when req.body.starRating is undefined
+    console.log("starRating is not defined");
+    res.json({
+      success: false,
+      message: "starRating is a required field",
+    });
+    return;
+  } else {
+    console.log("starRating is defined");
+    newMovie.starRating = req.body.starRating;
+  }
+
+  if (req.body.isRecommended === undefined) {
+    // Should trigger when req.body.isRecommended is undefined
+    console.log("isRecommended is not defined");
+    res.json({
+      success: false,
+      message: "isRecommended is a required field",
+    });
+    return;
+  } else {
+    console.log("isRecommended is defined");
+    newMovie.isRecommended = req.body.isRecommended;
+  }
+
+  console.log("newMovie ", newMovie);
+
+  favoriteMovieList.push(newMovie);
 
   // We must respond to the request, so for now we'll send back a hardcoded object
   res.json({
-    // success: true
+    success: true,
   });
 });
 
@@ -91,34 +147,91 @@ app.post("/new-movie", (req, res) => {
 
 // Get all the movies in our movie list
 app.get("/all-movies", (req, res) => {
+  console.log("GET to /all-movies");
+
   //res.send only sends strings. From now on, we want to use res.json to send JSON objects or JS arrays
 
   res.json(favoriteMovieList);
+});
+
+app.get("/single-movie/:titleToFind", (req, res) => {
+  const titleToFind = req.params.titleToFind;
+
+  const foundMovieIndex = favoriteMovieList.findIndex((movie) => {
+    if (movie.title === titleToFind) {
+      console.log("Movie Titles Match!");
+      return true;
+    } else {
+      console.log("Movie Titles Do Not Match");
+      return false;
+    }
+  });
+
+  const foundMovie = favoriteMovieList[foundMovieIndex];
+
+  res.json(foundMovie);
 });
 
 // Update
 
 // Find a movie and update the title
 app.put("/update-movie/:titleToUpdate", (req, res) => {
+  console.log("PUT to /update-movie");
+
   // We have a route parameter /:titleToUpdate to specify which movie in our list to update
   // The value of this route parameter will come through the req.params object
   console.log("req params ", req.params);
 
   const titleToUpdate = req.params.titleToUpdate;
-  const newTitle = req.body.newTitle;
 
-  console.log(titleToUpdate);
-  console.log(newTitle);
+  // We need to find the original movie in our movie array so that we can keep the original values that we don't want to modify. Hint: We need to use .findIndex()
+  const originalMovieIndex = favoriteMovieList.findIndex((movie) => {
+    console.log("movie ", movie);
+    console.log("titleToUpdate ", req.params.titleToUpdate);
+    console.log("condition ", movie.title === req.params.titleToUpdate);
 
-  console.log("favoriteMovieList before ", favoriteMovieList);
+    if (movie.title === req.params.titleToUpdate) {
+      console.log("Movie Titles Match!");
+      return true;
+    } else {
+      console.log("Movie Titles Do Not Match");
+      return false;
+    }
+  });
 
-  // In order to update the movie title we're targeting, first we find the index of the movie title in the array
-  const indexOfMovie = favoriteMovieList.indexOf(titleToUpdate);
-  console.log(indexOfMovie);
+  console.log("originalMovieIndex ", originalMovieIndex);
+
+  const originalMovie = favoriteMovieList[originalMovieIndex];
+
+  console.log("originalMovie ", originalMovie);
+
+  const updatedMovie = {
+    title: originalMovie.title,
+    starRating: originalMovie.starRating,
+    isRecommended: originalMovie.isRecommended,
+    createdAt: originalMovie.createdAt,
+    lastModified: new Date(),
+  };
+
+  console.log("updatedMovie Before Update ", updatedMovie);
+
+  if (req.body.title !== undefined) {
+    updatedMovie.title = req.body.title;
+  }
+
+  if (req.body.starRating !== undefined) {
+    updatedMovie.starRating = req.body.starRating;
+  }
+
+  if (req.body.isRecommended !== undefined) {
+    updatedMovie.isRecommended = req.body.isRecommended;
+  }
+
+  console.log("updatedMovie After Update ", updatedMovie);
 
   // Overwrite the value of favoriteMovieList at indexOfMovie with newTitle
-  favoriteMovieList[indexOfMovie] = newTitle;
-  console.log(favoriteMovieList);
+  // favoriteMovieList[indexOfMovie] = newTitle
+  favoriteMovieList[originalMovieIndex] = updatedMovie;
 
   console.log("favoriteMovieList after ", favoriteMovieList);
 
@@ -130,11 +243,23 @@ app.put("/update-movie/:titleToUpdate", (req, res) => {
 // Delete
 
 app.delete("/delete-movie/:titleToDelete", (req, res) => {
+  console.log("DELETE to /delete-movie");
+
   // This is the title of the movie we want to find in the movies array and delete
-  const titleToDelete = req.params.titleToDelete;
+  // const titleToDelete = req.params.titleToDelete
 
   // Find the index of the movie in the movie list
-  const indexOfMovie = favoriteMovieList.indexOf(titleToDelete);
+  const indexOfMovie = favoriteMovieList.findIndex((movie) => {
+    console.log(movie.title + " === " + req.params.titleToDelete);
+
+    if (movie.title === req.params.titleToDelete) {
+      console.log("Movie Titles Match!");
+      return true;
+    } else {
+      console.log("Movie Titles Do Not Match");
+      return false;
+    }
+  });
   console.log(indexOfMovie);
 
   if (indexOfMovie < 0) {
@@ -155,11 +280,11 @@ app.delete("/delete-movie/:titleToDelete", (req, res) => {
   });
 });
 
-/* app.get("/list-movies", (req, res) => {
-    const movieString = favoriteMovieList.join(", ");
-    console.log(`Some of my favorite movies are ${favoriteMovieList}`);
-    res.send(`Some of my favorite movies are ${movieString}`);
-});
+/* app.get("/list-movies", (req, res)=> {
+	const movieString = favoriteMovieList.join("; ")
+	console.log(favoriteMovieList)
+	res.send(movieString)
+})
 // Requesting a single resource of a set of resources is done with a route param
 // The url will be: localhost:3000/single-movie/Star Wars
 app.get("/single-movie/:movieName", (req, res)=> {
@@ -173,7 +298,6 @@ app.get("/single-movie/:movieName", (req, res)=> {
 		res.send("Movie not found!")
 	}
 })
-
 app.get("/add-movie", (req, res) => {
 	//localhost:3000/add-movie?newMovie=Terminator
 	console.log(req.query)
